5e224559c604fa06fd6f1cad507f942510ce1ade
audrey murua <audrey.murua1@gmail.com>
2021-10-13 10:37:34 -0500

    finished warmup 10-13-21

---
+++ js/codeup-warmups.js
@@ -0,0 +1,26 @@
+
+
+
+(function() {
+
+
+    function returnsSeven() {
+        return 7;
+    }
+
+    console.log(returnsSeven());
+
+//false if statement exercise
+    var age = 60;
+    var seniorDiscount = age >= 65;
+
+
+    if (seniorDiscount) {
+        console.log("Great! You get a discount!");
+    } else {
+        console.log("I'm sorry, you don't get a senior discount.");
+    }
+
+})();
+
+
