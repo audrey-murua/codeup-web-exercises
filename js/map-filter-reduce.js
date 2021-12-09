85f53b935ab9d8608e912049f2a8a1ddeb023467
audrey murua <audrey.murua1@gmail.com>
2021-12-09 09:44:07 -0600

    added more solutions and bonus to completed exercise

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -41,6 +41,11 @@ let languageArray = users.filter(language => language.languages.length === 3);
 
 console.log(languageArray)
 
+//other solution
+
+// let languageArray = users.filter((language) =>
+// language.languages.length > 2);
+
 //  Use .map to create an array of strings where each element is a user's email address
 
 let emailArray = users.map(email => email.email )
@@ -73,15 +78,65 @@ let longest = users.reduce((longEmail, allEmails) => {
 
 console.log(longest)
 
+//other solution
+
+// let longestEmail = users.reduce((startLength, addLetter) => startLength.email.length > addLetter.email.length ? startlength : addLetter);
+
+//other solution
+// let longestEmail = users.reduce((startLength, addLetter) => {
+//     if(startLength.email.length > addLetter.email.length) {
+//         return startLength;
+//     } else {
+//             return addLetter;
+//         }
+// })
+
+
 //     Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
 
 //Solution 1 using ternary
 
 // let instructorString = users.reduce(function(prevVal,currVal,idx){
 //     return idx == 0 ? currVal.name : prevVal + ', ' + currVal.name;
-// }, '')
+// }, 'Your instructors are: ')
 
-//Solution 2
+//Solution 2 not entirely correct
 
-let instructorString = users.reduce((prev, curr) => [prev, curr.name], []).join(', ');
-console.log(instructorString);
+// let instructorString = users.reduce((prev, curr) => [prev, curr.name], []).join(', ');
+// console.log(instructorString);
+
+//other solutions, not entirely correct
+
+// let instructors = users.reduce((previousValue, currentValue) => {
+//     if(!previousValue.includes(currentValue.name)) {
+//         previousValue.push(currentValue.name)
+//     }
+//     return previousValue;
+// },[]);
+
+// other solution, not entirely correct
+// let nameOfUsers = users.reduce((previousVal, currentVal ) => `${previousVal} ${currentVal.name}`, "Your instructors are: ")
+
+//other correct solution
+
+let instructors = users.reduce ((finalString, user, index) => {
+    if(index === users.length -1) {
+        //last index of the array = users.length -1
+        return `${finalString} ${user.name}.`
+    } else {
+        return `${finalString} ${user.name},`
+    }
+}, "Your instructors are: ")
+
+//Bonus Question
+
+let uniqueLangs = users.reduce((langList, user) => {
+   for (let lang of user.languages) {
+       if (!langList.includes(lang)) {
+           langList.push(lang);
+       }
+   }
+   return langList;
+},[])
+
+//[] on the bottom is used as a bucket of an empty array
